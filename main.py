2a8225ef6e48743676a688fc15357228a25f3315
Ádám László <adam.laszlo.91@gmail.com>
2024-10-08 18:15:52 +0200

    Reformat code

--- main.py
+++ main.py
@@ -21,15 +21,18 @@ def play_video() -> None:
         # Threshold the image then make the background transparent
         image = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         image = imutils.resize(image, height=18)
-        _, image = cv.threshold(src=image, thresh=127, maxval=255, type=cv.THRESH_BINARY)
-        image = cv.merge([image.copy(), image.copy(), image.copy(), image.copy()],4)
-        
+        _, image = cv.threshold(src=image, thresh=127,
+                                maxval=255, type=cv.THRESH_BINARY)
+        image = cv.merge([image.copy(), image.copy(),
+                         image.copy(), image.copy()], 4)
+
         # Write image to file, as rumps currently accepts only an image path
-        cv.imwrite(img= image, filename= 'frame.png')
+        cv.imwrite(img=image, filename='frame.png')
         app.icon = 'frame.png'
         time.sleep(0.04)
 
+
 if __name__ == '__main__':
-    thread = Thread(target = play_video)
+    thread = Thread(target=play_video)
     thread.start()
     app.run()
